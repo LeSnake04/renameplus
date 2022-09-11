7bae0940fa7a6687aa3a03ab78e5668888d86339
LeSnake04 <dev.lesnake@posteo.de>
2022-09-11 16:59:02 +0200

    Update arguments

--- src/args.rs
+++ src/args.rs
@@ -5,9 +5,7 @@ use clap::{value_parser, Arg, ArgMatches, Command};
 use clap_logger::{ClapLoglevelArg, LevelFilter};
 use_err!();
 
-// pub fn matches() -> Result<ArgMatches> {
 pub fn matches() -> ArgMatches {
-	// err!(
 	Command::new("autorename")
 		.about("Tool to rename files")
 		.arg_required_else_help(true)
@@ -19,6 +17,7 @@ pub fn matches() -> ArgMatches {
 				.short('p')
 				.value_name("PREFIX")
 				.required(false)
+				.help_heading("SIMPLE")
 				.help("Prefix to be added to the file"),
 		)
 		.arg(
@@ -32,20 +31,34 @@ pub fn matches() -> ArgMatches {
 		.arg(
 			Arg::new("dry")
 				.long("dry")
+				.short('d')
 				.help("Dont perfrom the operations"),
 		)
 		.arg(
-			Arg::new("allow-dirs")
+			Arg::new("dirs")
+				.long("dirs")
 				.short('r')
 				.help("Allow renaming of directories"),
 		)
 		.arg(
 			Arg::new("copy")
+				.long("copy")
 				.short('c')
 				.help("Copy files instead of moving them"),
 		)
+		.arg(
+			Arg::new("suffix")
+				.long("suffix")
+				.short('s')
+				.value_name("SUFFIX")
+				.help_heading("SIMPLE")
+				.help("Attach text to files"),
+		)
+		.arg(
+			Arg::new("fragile")
+				.long("fragile")
+				.short('f')
+				.help("Crash as soon as a error occurs"),
+		)
 		.get_matches()
-	// .try_get_matches(),
-	// "Failed to get Arguments"
-	// )
 }
